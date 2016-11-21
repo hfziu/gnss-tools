9d49f11e19aaffa618ad391ec2014826eb1d91f3
essethon <xcalcor@gmail.com>
2016-11-21 12:25:04 +0800

    rewrite geodetic

---
+++ geodetic/blh2xyz.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""Converts geodetic coordinates [B,L,H] to cartesian coordinates [X,Y,Z]
+author:essethon
+xcalcor@gmail.com
+MIT License
+
+Usage:
+    blh2xyz -e <ellipsoid> <B> <L> <H>
+
+Options:
+    -e <ellipsoid>  name of a particular reference ellipsoid
+
+Example:
+    blh2xyz -e 'WGS84' 50 50 0 
+"""
+
+import math
+from docopt import docopt
+
+from geo_ellipsoid import geo_ellipsoid
+
+def blh2xyz(ell, B, L, H):
+    a = geo_ellipsoid(ell).a
+    e2 = geo_ellipsoid(ell).e2
+    N = a / math.sqrt( 1 - e2 * math.sin(B) ** 2)
+    L = L / 180 * math.pi
+    B = B / 180 * math.pi
+
+    X = (N + H) * math.cos(B) * math.cos(L)
+    Y = (N + H) * math.cos(B) * math.sin(L)
+    Z = (N * (1 - e2) + H) * math.sin(B)
+
+    return [X, Y, Z]
+
+def cli():
+    arguments = docopt(__doc__)
+    #print(arguments)
+    ell = arguments.get('-e')
+    B = float(arguments.get('<B>'))
+    L = float(arguments.get('<L>'))
+    H = float(arguments.get('<H>'))
+    print(blh2xyz(ell, B, L, H))
+
+if __name__ == "__main__":
+    cli()
+
---
+++ geodetic/geo_ellipsoid.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+function geo_ellipsoid returns the semi-major axis, flattening and other parameters for
+a particular reference ellipsoid.
+"""
+import math
+
+class Ellipsoid:
+    def __init__(self, a, reciprocal_f):
+        self.a = a
+        self.f = 1/reciprocal_f
+        self.e2 = 1 - (1 - self.f) ** 2
+        self.omega = 7292115e-11 # rad/s
+        self.mu = 3.986004418e14 # m3/s2
+
+def geo_ellipsoid(ell):
+    try:
+        if ell == "Krasovsky":
+            return Ellipsoid(6378245.0, 298.2997381)
+        elif ell == "IUGG75" or ell == "IUGG-75":
+            return Ellipsoid(6378140.0, 298.257)
+        elif ell == "WGS84":
+            return Ellipsoid(6378137.0, 298.257223563)
+        elif ell == "CGCS2000":
+            return Ellipsoid(6378137.0, 298.257222101)
+        else:
+            raise NameError("Reference ellipsoid %s not found." % ell)
+    except NameError:
+        raise
